bc3d34caa8a9cbde0bfb7f61159d37a4c6ada9ee
Bharath B <98091989+bharathbdev@users.noreply.github.com>
2025-08-14 13:46:07 +0530

    test

---
+++ typescript_testing/test.js
@@ -0,0 +1,3 @@
+var input = "testing";
+var output = "Changes staged for commit";
+console.log("Changes staged for commit");
---
+++ typescript_testing/test.ts
@@ -0,0 +1,3 @@
+let input = `testing`;
+let output = "Changes staged for commit";
+console.log("Changes staged for commit");
